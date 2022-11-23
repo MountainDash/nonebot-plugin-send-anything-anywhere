48de5a3b09b04d6a58ab4c3ec105a37e057ffab5
felinae98 <731499577@qq.com>
2022-11-23 13:40:44 +0800

    :sparkles: add async converter

---
+++ tests/test_message_segment_convert.py
@@ -0,0 +1,125 @@
+import typing
+from typing import Type
+
+import pytest
+from nonebug import App
+
+from nonebot_plugin_send_anything_anywhere.utils import (
+    SupportedAdapters,
+    MessageSegmentFactory,
+)
+
+from .utils import assert_ms, make_fake_bot
+
+if typing.TYPE_CHECKING:
+    from nonebot_plugin_send_anything_anywhere.utils import (
+        SupportedAdapters,
+        MessageSegmentFactory,
+    )
+
+
+class Test(MessageSegmentFactory):
+    text: str
+
+    def __init__(self, text: str) -> None:
+        self.text = text
+        super().__init__()
+
+
+@pytest.fixture
+def dummy_factory(app: App):
+    class _Test(Test):
+        pass
+
+    return _Test
+
+
+@pytest.fixture
+def onebot_v11(app: App):
+    from nonebot_plugin_send_anything_anywhere.utils import SupportedAdapters
+
+    return SupportedAdapters.onebot_v11
+
+
+async def test_sync_without_bot(
+    app: App, dummy_factory: "Type[Test]", onebot_v11: "SupportedAdapters"
+):
+    from nonebot.adapters.onebot.v11.bot import Bot
+    from nonebot.adapters.onebot.v11.message import MessageSegment
+
+    from nonebot_plugin_send_anything_anywhere.utils import register_ms_adapter
+
+    @register_ms_adapter(onebot_v11, dummy_factory)
+    def _text(t):
+        return MessageSegment.text("314159")
+
+    await assert_ms(
+        Bot,
+        SupportedAdapters.onebot_v11,
+        app,
+        dummy_factory("314159"),
+        MessageSegment.text("314159"),
+    )
+
+
+async def test_sync_with_bot(
+    app: App, dummy_factory: "Type[Test]", onebot_v11: "SupportedAdapters"
+):
+    from nonebot.adapters.onebot.v11.bot import Bot
+    from nonebot.adapters.onebot.v11.message import MessageSegment
+
+    from nonebot_plugin_send_anything_anywhere.utils import register_ms_adapter
+
+    @register_ms_adapter(onebot_v11, dummy_factory)
+    def _text(t, bot):
+        return MessageSegment.text("314159")
+
+    await assert_ms(
+        Bot,
+        SupportedAdapters.onebot_v11,
+        app,
+        dummy_factory("314159"),
+        MessageSegment.text("314159"),
+    )
+
+
+async def test_async_without_bot(
+    app: App, dummy_factory: "Type[Test]", onebot_v11: "SupportedAdapters"
+):
+    from nonebot.adapters.onebot.v11.bot import Bot
+    from nonebot.adapters.onebot.v11.message import MessageSegment
+
+    from nonebot_plugin_send_anything_anywhere.utils import register_ms_adapter
+
+    @register_ms_adapter(onebot_v11, dummy_factory)
+    async def _text(t):
+        return MessageSegment.text("314159")
+
+    await assert_ms(
+        Bot,
+        SupportedAdapters.onebot_v11,
+        app,
+        dummy_factory("314159"),
+        MessageSegment.text("314159"),
+    )
+
+
+async def test_async_with_bot(
+    app: App, dummy_factory: "Type[Test]", onebot_v11: "SupportedAdapters"
+):
+    from nonebot.adapters.onebot.v11.bot import Bot
+    from nonebot.adapters.onebot.v11.message import MessageSegment
+
+    from nonebot_plugin_send_anything_anywhere.utils import register_ms_adapter
+
+    @register_ms_adapter(onebot_v11, dummy_factory)
+    async def _text(t, bot):
+        return MessageSegment.text("314159")
+
+    await assert_ms(
+        Bot,
+        SupportedAdapters.onebot_v11,
+        app,
+        dummy_factory("314159"),
+        MessageSegment.text("314159"),
+    )
