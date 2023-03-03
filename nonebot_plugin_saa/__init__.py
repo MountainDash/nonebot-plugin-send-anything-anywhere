dea0a00ee34d4bcfcb3ad151cc81d2897ae9e845
felinae98 <731499577@qq.com>
2023-03-03 20:40:41 +0800

    :sparkles: introduce overwrite method

--- nonebot_plugin_saa/__init__.py
+++ nonebot_plugin_saa/__init__.py
@@ -1,5 +1,13 @@
-from .utils import MessageFactory
 from . import adapters  # noqa: F401
+from .utils import MessageFactory, SupportedAdapters
 from .types import Text, Image, Reply, Custom, Mention
 
-__all__ = ["Text", "Image", "Mention", "Reply", "Custom", "MessageFactory"]
+__all__ = [
+    "Text",
+    "Image",
+    "Mention",
+    "Reply",
+    "Custom",
+    "MessageFactory",
+    "SupportedAdapters",
+]
--- nonebot_plugin_saa/types/custom_message_segment.py
+++ nonebot_plugin_saa/types/custom_message_segment.py
@@ -1,24 +1,21 @@
-from typing import Callable
-
 from nonebot.adapters import Bot, MessageSegment
 
 from ..utils import (
+    CustomBuildFunc,
     SupportedAdapters,
     AdapterNotSupported,
     MessageSegmentFactory,
+    do_build_custom,
     extract_adapter_type,
 )
 
 
 class Custom(MessageSegmentFactory):
     "用户自定义的 MessageSegment"
-    ms_dict: dict[SupportedAdapters, MessageSegment | Callable[[Bot], MessageSegment]]
 
     def __init__(
         self,
-        ms_dict: dict[
-            SupportedAdapters, MessageSegment | Callable[[Bot], MessageSegment]
-        ],
+        ms_dict: dict[SupportedAdapters, MessageSegment | CustomBuildFunc],
     ):
         """
         自定义 MessageSegment
@@ -28,15 +25,14 @@ class Custom(MessageSegmentFactory):
                      val 为 MessageSegment 或 Bot -> MessageSegment 的函数，
                      规定为各个适配器返回的 MessageSegment
         """
-        self.ms_dict = ms_dict
+        super().__init__()
+        for k, v in ms_dict.items():
+            self._register_custom_builder(k, v)
 
     async def build(self, bot: Bot) -> MessageSegment:
         adapter_name = extract_adapter_type(bot)
-        if adapter_name not in self.ms_dict.keys():
+        if not (ms_builder := self._custom_builders.get(adapter_name)):
             raise AdapterNotSupported(adapter_name)
 
-        ms = self.ms_dict[adapter_name]
-        if not isinstance(ms, MessageSegment):
-            ms = ms(bot)
-
+        ms = await do_build_custom(ms_builder, bot)
         return ms
