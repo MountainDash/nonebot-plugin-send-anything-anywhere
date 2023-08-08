288ec49d0b900545535b8f2d3cd6d3aee4f5436b
uy_sun <hmy0119@gmail.com>
2023-08-08 23:17:17 +0800

    :art: Use re-export format

--- nonebot_plugin_saa/types/__init__.py
+++ nonebot_plugin_saa/types/__init__.py
@@ -1,10 +1,5 @@
-from .custom_message_segment import Custom
-from .common_message_segment import Text, Image, Reply, Mention
-
-__all__ = [
-    "Text",
-    "Image",
-    "Mention",
-    "Reply",
-    "Custom",
-]
+from .common_message_segment import Text as Text
+from .common_message_segment import Image as Image
+from .common_message_segment import Reply as Reply
+from .custom_message_segment import Custom as Custom
+from .common_message_segment import Mention as Mention
--- nonebot_plugin_saa/utils/__init__.py
+++ nonebot_plugin_saa/utils/__init__.py
@@ -1,79 +1,39 @@
-from .helpers import extract_adapter_type
-from .exceptions import AdapterNotInstalled, AdapterNotSupported
-from .auto_select_bot import register_list_targets, enable_auto_select_bot
-from .const import SupportedAdapters, SupportedPlatform, supported_adapter_names
-from .types import (
-    BuildFunc,
-    MessageFactory,
-    CustomBuildFunc,
-    MessageSegmentFactory,
-    AggregatedMessageFactory,
-    do_build,
-    do_build_custom,
-    register_ms_adapter,
-    assamble_message_factory,
-)
+from .types import do_build as do_build
+from .types import BuildFunc as BuildFunc
+from .types import MessageFactory as MessageFactory
+from .types import CustomBuildFunc as CustomBuildFunc
+from .types import do_build_custom as do_build_custom
+from .const import SupportedAdapters as SupportedAdapters
+from .const import SupportedPlatform as SupportedPlatform
+from .platform_send_target import get_target as get_target
+from .types import register_ms_adapter as register_ms_adapter
+from .platform_send_target import TargetQQGroup as TargetQQGroup
+from .helpers import extract_adapter_type as extract_adapter_type
+from .types import MessageSegmentFactory as MessageSegmentFactory
+from .exceptions import AdapterNotInstalled as AdapterNotInstalled
+from .exceptions import AdapterNotSupported as AdapterNotSupported
+from .platform_send_target import PlatformTarget as PlatformTarget
+from .platform_send_target import extract_target as extract_target
+from .platform_send_target import TargetQQPrivate as TargetQQPrivate
+from .platform_send_target import register_sender as register_sender
+from .const import supported_adapter_names as supported_adapter_names
+from .platform_send_target import TargetOB12Unknow as TargetOB12Unknow
+from .types import AggregatedMessageFactory as AggregatedMessageFactory
+from .types import assamble_message_factory as assamble_message_factory
+from .platform_send_target import QQGuildDMSManager as QQGuildDMSManager
+from .platform_send_target import TargetFeishuGroup as TargetFeishuGroup
+from .auto_select_bot import register_list_targets as register_list_targets
+from .platform_send_target import TargetFeishuPrivate as TargetFeishuPrivate
+from .platform_send_target import TargetQQGuildDirect as TargetQQGuildDirect
+from .platform_send_target import TargetTelegramForum as TargetTelegramForum
+from .auto_select_bot import enable_auto_select_bot as enable_auto_select_bot
+from .platform_send_target import TargetQQGuildChannel as TargetQQGuildChannel
+from .platform_send_target import TargetTelegramCommon as TargetTelegramCommon
+from .platform_send_target import register_qqguild_dms as register_qqguild_dms
+from .platform_send_target import TargetKaiheilaChannel as TargetKaiheilaChannel
+from .platform_send_target import TargetKaiheilaPrivate as TargetKaiheilaPrivate
+from .platform_send_target import register_convert_to_arg as register_convert_to_arg
+from .platform_send_target import register_target_extractor as register_target_extractor
 from .platform_send_target import (
-    TargetQQGroup,
-    PlatformTarget,
-    TargetQQPrivate,
-    TargetOB12Unknow,
-    QQGuildDMSManager,
-    TargetFeishuGroup,
-    TargetFeishuPrivate,
-    TargetQQGuildDirect,
-    TargetTelegramForum,
-    TargetQQGuildChannel,
-    TargetTelegramCommon,
-    TargetKaiheilaChannel,
-    TargetKaiheilaPrivate,
-    AllSupportedPlatformTarget,
-    get_target,
-    extract_target,
-    register_sender,
-    register_qqguild_dms,
-    register_convert_to_arg,
-    register_target_extractor,
+    AllSupportedPlatformTarget as AllSupportedPlatformTarget,
 )
-
-__all__ = [
-    "SupportedAdapters",
-    "SupportedPlatform",
-    "supported_adapter_names",
-    "AdapterNotInstalled",
-    "AdapterNotSupported",
-    "MessageSegmentFactory",
-    "MessageFactory",
-    "AggregatedMessageFactory",
-    "register_ms_adapter",
-    "extract_adapter_type",
-    "BuildFunc",
-    "CustomBuildFunc",
-    "do_build",
-    "do_build_custom",
-    "extract_target",
-    "get_target",
-    "register_target_extractor",
-    "register_sender",
-    "assamble_message_factory",
-    "register_convert_to_arg",
-    "register_target_extractor",
-    "PlatformTarget",
-    "TargetQQGroup",
-    "TargetQQPrivate",
-    "TargetQQGuildChannel",
-    "TargetQQGuildDirect",
-    "TargetKaiheilaChannel",
-    "TargetKaiheilaPrivate",
-    "TargetFeishuPrivate",
-    "TargetFeishuGroup",
-    "TargetOB12Unknow",
-    "TargetTelegramCommon",
-    "TargetTelegramForum",
-    "AllSupportedPlatformTarget",
-    "auto_select_bot",
-    "register_list_targets",
-    "enable_auto_select_bot",
-    "register_qqguild_dms",
-    "QQGuildDMSManager",
-]
