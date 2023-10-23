6a862484911d7fc8cdb206e9b8fd15d370baa381
uy_sun <hmy0119@gmail.com>
2023-10-23 22:01:38 +0800

    :construction: 支持 unknown_satori

--- nonebot_plugin_saa/utils/const.py
+++ nonebot_plugin_saa/utils/const.py
@@ -25,6 +25,7 @@ class SupportedPlatform(StrEnum):
     kaiheila_channel = "Kaiheila Channel"
     kaiheila_private = "Kaiheila Private"
     unknown_ob12 = "Unknow Onebot 12 Platform"
+    unknown_satori = "Unknown Satori Platform"
     telegram_common = "Telegram Common"
     telegram_forum = "Telegram Forum"
     feishu_private = "Feishu Private"
