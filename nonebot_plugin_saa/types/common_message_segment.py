b60513621826a58290402a79303647f547a4bbab
Azide <rukuy@qq.com>
2023-12-28 23:07:16 +0800

    :fire: 移除不再需要的type字段

--- nonebot_plugin_saa/types/common_message_segment.py
+++ nonebot_plugin_saa/types/common_message_segment.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 from pathlib import Path
-from typing import Union, Literal, TypedDict
+from typing import Union, TypedDict
 
 from ..registries import MessageId
 from ..abstract_factories import MessageFactory, MessageSegmentFactory
@@ -13,7 +13,6 @@ class TextData(TypedDict):
 class Text(MessageSegmentFactory):
     """文本消息段"""
 
-    type: Literal["text"] = "text"
     data: TextData
 
     def __init__(self, text: str) -> None:
@@ -43,7 +42,6 @@ class ImageData(TypedDict):
 class Image(MessageSegmentFactory):
     """图片消息段"""
 
-    type: Literal["image"] = "image"
     data: ImageData
 
     def __init__(
@@ -101,7 +99,6 @@ class MentionData(TypedDict):
 class Mention(MessageSegmentFactory):
     """提到其他用户"""
 
-    type: Literal["mention"] = "mention"
     data: MentionData
 
     def __init__(self, user_id: str):
@@ -118,7 +115,6 @@ class Mention(MessageSegmentFactory):
 class Reply(MessageSegmentFactory):
     """回复其他消息的消息段"""
 
-    type: Literal["reply"] = "reply"
     data: MessageId
 
     def __init__(self, message_id: MessageId):
