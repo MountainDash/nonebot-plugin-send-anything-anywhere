0622a6df7a1cbb7db7d1de3ca437f8314ca96ead
Azide <rukuy@qq.com>
2024-01-20 00:19:50 +0800

    :arrow_up: upgrade docusaurus to 3

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -1,8 +1,8 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
+const { themes } = require("prism-react-renderer");
+const lightCodeTheme = themes.github;
+const darkCodeTheme = themes.dracula;
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -147,6 +147,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
+        additionalLanguages: ["python", "bash", "diff", "json"],
       },
     }),
 };
