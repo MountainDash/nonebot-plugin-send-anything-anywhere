1e304835509cdbd3f06208c956ab900f02b9b9f6
uy/sun <hmy0119@gmail.com>
2024-02-14 23:53:59 +0800

    :fire: 移除 qqguild 适配器 (#148)

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ from nonebot.adapters.qq import Adapter as QQAdapter
 from nonebot.adapters.red import Adapter as RedAdapter
 from nonebot.adapters.dodo import Adapter as DoDoAdapter
 from nonebot.adapters.feishu import Adapter as FeishuAdapter
-from nonebot.adapters.qqguild import Adapter as QQGuildAdapter
 from nonebot.adapters.telegram import Adapter as TelegramAdapter
 from nonebot.adapters.onebot.v11 import Adapter as OnebotV11Adapter
 from nonebot.adapters.onebot.v12 import Adapter as OnebotV12Adapter
@@ -30,7 +29,6 @@ def load_adapters(nonebug_init: None):
     driver = nonebot.get_driver()
     driver.register_adapter(OnebotV11Adapter)
     driver.register_adapter(OnebotV12Adapter)
-    driver.register_adapter(QQGuildAdapter)
     driver.register_adapter(TelegramAdapter)
     driver.register_adapter(FeishuAdapter)
     driver.register_adapter(RedAdapter)
