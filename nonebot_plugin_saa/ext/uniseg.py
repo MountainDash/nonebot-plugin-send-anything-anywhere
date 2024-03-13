4245b2e1f04535bd08dd41aec3ef83a6b63418b4
Azide <rukuy@qq.com>
2024-03-13 20:50:24 +0800

    :recycle: 优化一下

--- nonebot_plugin_saa/ext/uniseg.py
+++ nonebot_plugin_saa/ext/uniseg.py
@@ -1,57 +1,147 @@
 try:
-    from nonebot_plugin_alconna.uniseg import Other, Segment
+    from nonebot_plugin_alconna.uniseg import Text, Other, Custom
     from nonebot_plugin_alconna.uniseg import UniMessage as UniMsg
-    from nonebot_plugin_alconna.uniseg.adapters import MAPPING as alc_exportor_map
+    from nonebot_plugin_alconna.uniseg import Segment as AlcSegment
+    from nonebot_plugin_alconna.uniseg.adapters import (
+        EXPORTER_MAPPING as ALC_EXPORTER_MAPPING,
+    )
 except ImportError as e:
     raise ImportError(
         "请使用 `pip install nonebot-plugin-send-anything-anywhere[alc]` 安装所需依赖"
     ) from e
 
+from functools import partial
 from typing_extensions import override
-from typing import Union, TypeVar, Iterable
+from typing import Union, TypeVar, Iterable, TypedDict
 
 from nonebot.adapters import Bot, Message, MessageSegment
 
-from ..utils import extract_adapter_type
+from ..utils import SupportedAdapters, extract_adapter_type
 from ..abstract_factories import MessageFactory, MessageSegmentFactory
 
-UMS = TypeVar("UMS", bound=Segment)
+AMS = TypeVar("AMS", bound=AlcSegment)
 
 
-class UniSegmentUnsupport(Exception):
+class UnisegExtException(Exception):
     ...
 
 
-class UniMessageSegmentFactory(MessageSegmentFactory):
-    data: Segment
-
-    def __init__(self, uniseg: Segment) -> None:
-        self.data = uniseg
-
-    async def build(self, bot: Bot) -> MessageSegment:
-        adapter_name = extract_adapter_type(bot)
-        seg_type = self.data.__class__
-        export_res = None
-        if exportor := alc_exportor_map.get(adapter_name):
-            exportor.segment_class = exportor.get_message_type().get_segment_class()
-            export_res = await exportor._mapping[seg_type](self.data, bot)
-
-        if not export_res and isinstance(self.data, Other):
-            export_res = self.data.origin
-
-        if not export_res:
-            raise UniSegmentUnsupport(self.data)
-
-        return export_res
+class AlcSegmentBuildError(UnisegExtException):
+    @classmethod
+    def from_seg(cls, seg: AlcSegment, msg: str):
+        return cls(f"Alc Segment [{seg.__class__}] -> {seg} <-: {msg}")
 
 
-class UniMessageFactory(MessageFactory):
+class UniSegmentUnsupport(UnisegExtException):
+    ...
+
+
+class UniMsgData(TypedDict):
+    uniseg: AlcSegment
+    fallback: bool
+
+
+async def alc_builder(data: UniMsgData, bot: Bot):
+    """抄自nonebot_plugin_alconna.uniseg.exporter.MessageExporter.export"""
+    seg = data["uniseg"]
+    fallback = data["fallback"]
+    adapter_name = extract_adapter_type(bot)
+    seg_type = seg.__class__
+
+    if not (exportor := ALC_EXPORTER_MAPPING.get(adapter_name)):
+        raise AlcSegmentBuildError.from_seg(seg, f"在 {adapter_name} 上找不到对应的 Exportor")
+
+    if seg_type in exportor._mapping:
+        export_result = await exportor._mapping[seg_type](seg, bot)
+        if isinstance(export_result, list):
+            raise AlcSegmentBuildError.from_seg(
+                seg,
+                f"在 {adapter_name} 中构建出多个 MessageSegment, SAA 尚未支持其转换",
+            )
+
+        return export_result
+
+    elif isinstance(seg, Custom):
+        msg_type = exportor.get_message_type()
+        return seg.export(msg_type)
+
+    elif isinstance(seg, Other):
+        return seg.origin
+
+    elif fallback:
+        if not isinstance(
+            res := alc_builder(UniMsgData(uniseg=Text(str(seg)), fallback=False), bot),
+            MessageSegment,
+        ):
+            raise AlcSegmentBuildError.from_seg(seg, "fallback 失败！")
+        else:
+            return res
+    else:
+        raise AlcSegmentBuildError.from_seg(seg, "不支持构建")
+
+
+class AlcMessageSegmentFactory(MessageSegmentFactory):
+    data: UniMsgData
+
+    def __init__(self, uniseg: AlcSegment, fallback: bool = False) -> None:
+        self.data = UniMsgData(uniseg=uniseg, fallback=fallback)
+        self._init_alc_builder()
+        super().__init__()
+
+    def _init_alc_builder(self):
+        def maker(msf: "AlcMessageSegmentFactory", bot: Bot):
+            return alc_builder(data=msf.data, bot=bot)
+
+        for adapter_name in SupportedAdapters:
+            self._builders[adapter_name] = maker
+
+    @classmethod
+    def from_unimsg(cls, msg: UniMsg, fallback: bool = False):
+        return [*map(partial(cls, fallback=fallback), msg)]
+
+    @classmethod
+    def from_str(cls, msg: str, fallback: bool = False):
+        return cls(Text(msg), fallback=fallback)
+
+
+class AlcMessageFactory(MessageFactory):
     def __init__(
-        self, message: Union[Iterable[Union[str, UMS]], str, UMS, None] = None
+        self,
+        message: Union[Iterable[Union[str, AMS]], str, AMS, None] = None,
+        fallback: bool = False,
     ):
         self.unimsg = UniMsg(message)
-        self.extend(map(UniMessageSegmentFactory, self.unimsg))
+        AMSFWithFallback = partial(AlcMessageSegmentFactory, fallback=fallback)
+        self.extend(map(AMSFWithFallback, self.unimsg))
+        super().__init__()
 
     @override
     async def build(self, bot: Bot) -> Message:
         return await self.unimsg.export(bot)
+
+
+class UniMessageFactory(MessageFactory):
+    def __init__(
+        self,
+        message: "str | MessageSegmentFactory | AlcSegment | Iterable[str | MessageSegmentFactory | AlcSegment] | None" = None,  # noqa: E
+        fallback: bool = False,
+    ):
+        if isinstance(message, AlcSegment):
+            amessage = AlcMessageSegmentFactory(message)
+        elif isinstance(message, UniMsg):
+            amessage = map(partial(AlcMessageSegmentFactory, fallback=fallback), message)  # type: ignore 已经在 UniMsg 内部全部转换为 uniseg 了  # noqa: E501
+        elif isinstance(message, Iterable):
+
+            def convert(m: "str | MessageSegmentFactory | AlcSegment"):
+                if isinstance(m, AlcSegment):
+                    return AlcMessageSegmentFactory(m, fallback=fallback)
+                else:
+                    return m
+
+            amessage = map(convert, message)
+
+        super().__init__(amessage)
+
+    @classmethod
+    def from_unimsg(cls, msg: UniMsg, fallback: bool = False):
+        return cls(map(partial(AlcMessageSegmentFactory, fallback=fallback), msg))
--- tests/test_alconna_uniseg.py
+++ tests/test_alconna_uniseg.py
@@ -4,10 +4,19 @@ from .utils import ob12_kwargs, mock_obv12_message_event
 
 
 async def test_send(app: App):
-    from nonebot_plugin_alconna import Text
     from nonebot import get_driver, on_message
-    from nonebot.adapters.onebot.v12 import Bot, Message, PrivateMessageEvent
+    from nonebot_plugin_alconna import File as AF
+    from nonebot_plugin_alconna import Text as AT
+    from nonebot_plugin_alconna import Image as AI
+    from nonebot.adapters.onebot.v12 import (
+        Bot,
+        Message,
+        MessageSegment,
+        PrivateMessageEvent,
+    )
 
+    from nonebot_plugin_saa import Text as ST
+    from nonebot_plugin_saa import Image as SI
     from nonebot_plugin_saa import SupportedAdapters
     from nonebot_plugin_saa.ext.uniseg import UniMessageFactory
 
@@ -15,7 +24,13 @@ async def test_send(app: App):
 
     @matcher.handle()
     async def process(msg: PrivateMessageEvent):
-        await UniMessageFactory([Text("123"), Text("456")]).send()
+        await UniMessageFactory([AT("#1 alc t1"), AT("#1 alc t2")]).send()
+        await UniMessageFactory([ST("#2 saa t1"), ST("#2 saa t2")]).send()
+        await UniMessageFactory([AT("#3 alc t1"), ST("#3 saa t1")]).send()
+        await UniMessageFactory([AT("#4 alc t1"), SI("#4 saa i1")]).send()
+        await UniMessageFactory([AI(url="https://a.lc/i1#5"), ST("#5 saa t1")]).send()
+        await UniMessageFactory([AI(url="https://a/lc/i1#6"), SI(b"#6 saa t1")]).send()
+        await UniMessageFactory([AF(url="https://a.lc/f1#7"), ST("#7 saa t1")]).send()
 
     async with app.test_matcher(matcher) as ctx:
         adapter_obj = get_driver()._adapters[str(SupportedAdapters.onebot_v12)]
@@ -24,12 +39,124 @@ async def test_send(app: App):
         )
         msg_event = mock_obv12_message_event(Message("321"))
         ctx.receive_event(bot, msg_event)
+        # 1
         ctx.should_call_api(
             "send_message",
             data={
-                "message": Message("123456"),
+                "message": Message(
+                    [MessageSegment.text("#1 alc t1"), MessageSegment.text("#1 alc t2")]
+                ),
                 "user_id": "2233",
                 "detail_type": "private",
             },
-            result={"message_id": 12451},
+            result={"message_id": "1"},
+        )
+        # 2
+        ctx.should_call_api(
+            "send_message",
+            data={
+                "message": Message(
+                    [MessageSegment.text("#2 saa t1"), MessageSegment.text("#2 saa t2")]
+                ),
+                "user_id": "2233",
+                "detail_type": "private",
+            },
+            result={"message_id": "2"},
+        )
+        # 3
+        ctx.should_call_api(
+            "send_message",
+            data={
+                "message": Message(
+                    [MessageSegment.text("#3 alc t1"), MessageSegment.text("#3 saa t1")]
+                ),
+                "user_id": "2233",
+                "detail_type": "private",
+            },
+            result={"message_id": "3"},
+        )
+        # 4
+        ctx.should_call_api(
+            "upload_file",
+            {"type": "url", "name": "image", "url": "#4 saa i1"},
+            {"file_id": "#4 saa i1 id"},
+        )
+        ctx.should_call_api(
+            "send_message",
+            data={
+                "message": Message(
+                    [
+                        MessageSegment.text("#4 alc t1"),
+                        MessageSegment.image("#4 saa i1 id"),
+                    ]
+                ),
+                "user_id": "2233",
+                "detail_type": "private",
+            },
+            result={"message_id": "4"},
+        )
+        # 5
+        ctx.should_call_api(
+            "upload_file",
+            {"type": "url", "name": "image.png", "url": "https://a.lc/i1#5"},
+            {"file_id": "#5 alc i1 id"},
+        )
+        ctx.should_call_api(
+            "send_message",
+            data={
+                "message": Message(
+                    [
+                        MessageSegment.image("#5 alc i1 id"),
+                        MessageSegment.text("#5 saa t1"),
+                    ]
+                ),
+                "user_id": "2233",
+                "detail_type": "private",
+            },
+            result={"message_id": "5"},
+        )
+        # 6
+        ctx.should_call_api(
+            "upload_file",
+            {"type": "url", "name": "image.png", "url": "https://a/lc/i1#6"},
+            {"file_id": "#6 alc i1 id"},
+        )
+        ctx.should_call_api(
+            "upload_file",
+            {"type": "data", "name": "image", "data": b"#6 saa t1"},
+            {"file_id": "#6 saa i1 id"},
+        )
+        ctx.should_call_api(
+            "send_message",
+            data={
+                "message": Message(
+                    [
+                        MessageSegment.image("#6 alc i1 id"),
+                        MessageSegment.image(file_id="#6 saa i1 id"),
+                    ]
+                ),
+                "user_id": "2233",
+                "detail_type": "private",
+            },
+            result={"message_id": "6"},
+        )
+        # # 7
+        ctx.should_call_api(
+            "upload_file",
+            {"type": "url", "name": "file.bin", "url": "https://a.lc/f1#7"},
+            {"file_id": "#7 alc f1 id"},
+        )
+        ctx.should_call_api(
+            "send_message",
+            data={
+                "message": Message(
+                    [
+                        MessageSegment.file("#7 alc f1 id"),
+                        MessageSegment.text("#7 saa t1"),
+                    ]
+                ),
+                "user_id": "2233",
+                "detail_type": "private",
+            },
+            result={"message_id": "7"},
         )
