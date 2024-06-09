9dfc4675e8184c340f251b7c6ad18765c5d26512
Azide <rukuy@qq.com>
2024-06-10 03:57:38 +0800

    :sparkles: 适配最新的saa

--- nonebot_plugin_saa/adapters/discord.py
+++ nonebot_plugin_saa/adapters/discord.py
@@ -1,30 +1,44 @@
 from io import BytesIO
 from pathlib import Path
-from typing import Any, Dict
 from functools import partial
+from contextlib import suppress
+from typing import Any, Dict, List, Optional, cast
 
 from nonebot.adapters import Event
+from nonebot.drivers import Request
 from nonebot.adapters import Bot as BaseBot
-
-from ..types import Text, Image, Reply, Mention
-from ..utils import (
-    MessageFactory,
-    PlatformTarget,
-    SupportedAdapters,
-    SupportedPlatform,
-    TargetDiscordChannel,
-    MessageSegmentFactory,
-    register_sender,
-    register_ms_adapter,
-    register_convert_to_arg,
-    assamble_message_factory,
-    register_target_extractor,
+from nonebot.adapters.discord.api.model import (
+    File,
+    Embed,
+    MessageFlag,
+    AllowedMention,
+    AttachmentSend,
+    DirectComponent,
 )
 
-try:
-    import aiohttp
-    from nonebot.adapters.discord import Bot
+from ..auto_select_bot import register_list_targets
+from ..utils import SupportedAdapters, SupportedPlatform
+from ..types import Text, Image, Reply, Mention, MentionAll
+from ..abstract_factories import (
+    MessageFactory,
+    register_ms_adapter,
+    assamble_message_factory,
+)
+from ..registries import (
+    Receipt,
+    MessageId,
+    PlatformTarget,
+    TargetDiscordChannel,
+    register_sender,
+    register_convert_to_arg,
+    register_target_extractor,
+    register_message_id_getter,
+)
+
+with suppress(ImportError):
+    from nonebot.adapters.discord import Bot as BotDiscord
     from nonebot.adapters.discord.message import Message, MessageSegment
+    from nonebot.adapters.discord.api.model import Snowflake, MessageGet, SnowflakeType
     from nonebot.adapters.discord.event import (
         MessageEvent,
         MessageCreateEvent,
@@ -36,57 +50,81 @@ try:
 
     MessageFactory.register_adapter_message(SupportedAdapters.discord, Message)
 
+    class DiscordMessageId(MessageId):
+        adapter_name: SupportedAdapters = adapter
+        message_id: SnowflakeType
+        channel_id: Optional[SnowflakeType] = None
+
+    @register_message_id_getter(MessageEvent)
+    def _get_msg_id(event: Event) -> DiscordMessageId:
+        assert isinstance(event, MessageEvent)
+        return DiscordMessageId(
+            message_id=event.message_id, channel_id=event.channel_id
+        )
+
     @register_discord(Text)
     def _text(t: Text) -> MessageSegment:
         return MessageSegment.text(t.data["text"])
 
     @register_discord(Image)
     async def _image(i: Image, bot: BaseBot) -> MessageSegment:
-        if not isinstance(bot, Bot):
+        if not isinstance(bot, BotDiscord):
             raise TypeError(f"Unsupported type of bot: {type(bot)}")
         image = i.data["image"]
-        img_bytes = b""
-        if isinstance(image, (str, Path)):
-            path = Path(image)
-            if i.data["name"] == "image":
-                if path.suffix not in [".jpg", ".jpeg", ".png", ".gif"]:
-                    i.data["name"] = path.with_suffix(".png").name
-                else:
-                    i.data["name"] = path.name
-            if path.exists():
-                try:
-                    with path.open("rb") as f:
-                        img_bytes = f.read()
-                except Exception as error:
-                    raise Exception(f"Error reading local image: {str(error)}")
+        image_name = i.data["name"]
+
+        if isinstance(image, Path) and image.is_file():
+            if image_name == "image" and image.suffix not in [
+                ".jpg",
+                ".jpeg",
+                ".png",
+                ".gif",
+            ]:
+                image_name = image.with_suffix(".png").name
             else:
-                async with aiohttp.ClientSession() as session:
-                    async with session.get(str(image)) as response:
-                        if response.status == 200:
-                            img_bytes = await response.read()
-                        else:
-                            raise Exception(
-                                f"Error downloading image, status code: {response.status}"  # noqa: E501
-                            )
+                image_name = image.name
+
+            with image.open("rb") as f:
+                img_bytes = f.read()
+
+        elif isinstance(image, str):
+            req = Request("GET", image, timeout=10)
+            resp = await bot.adapter.request(req)
+            if resp.status_code != 200:
+                raise RuntimeError(
+                    f"Error downloading image, status code: {resp.status_code}, url: {image}"  # noqa: E501
+                )
+            img_bytes = resp.content
+            if not isinstance(img_bytes, bytes):
+                raise TypeError(f"Expected bytes, got something else {type(img_bytes)}")
+
         elif isinstance(image, bytes):
             img_bytes = image
+
         elif isinstance(image, BytesIO):
-            img_bytes = image.read()
+            img_bytes = image.getvalue()
+
         else:
-            raise TypeError("Invalid image type")
+            raise TypeError(f"Invalid image type {type(image)}")
 
         return MessageSegment.attachment(
             content=img_bytes,
-            file=i.data["name"] if i.data["name"] != "image" else "image.png",
+            file=image_name,
         )
 
-    @register_discord(Mention)
-    async def _mention(m: Mention) -> MessageSegment:
-        return MessageSegment.mention_user(user_id=int(m.data["user_id"]))
-
     @register_discord(Reply)
-    async def _reply(r: Reply) -> MessageSegment:
-        return MessageSegment.reference(reference=int(r.data["message_id"]))
+    def _reply(r: Reply) -> MessageSegment:
+        assert isinstance(mid := r.data["message_id"], DiscordMessageId)
+        return MessageSegment.reference(reference=mid.message_id)
+
+    @register_discord(Mention)
+    def _mention(m: Mention) -> MessageSegment:
+        return MessageSegment.mention_user(user_id=Snowflake(m.data["user_id"]))
+
+    @register_discord(MentionAll)
+    def _mention_all(m: MentionAll) -> MessageSegment:
+        # TODO: 限定可以@的范围（channel等）
+        return MessageSegment.mention_everyone()
 
     @register_target_extractor(ChannelPinsUpdateEvent)
     @register_target_extractor(MessageCreateEvent)
@@ -102,23 +140,91 @@ try:
             "channel_id": target.channel_id,
         }
 
-    @register_sender(SupportedAdapters.discord)
+    class DiscordReceipt(Receipt):
+        adapter_name: SupportedAdapters = adapter
+        message_get: MessageGet
+
+        async def revoke(self, reason: Optional[str] = None):
+            return await cast(BotDiscord, self._get_bot()).delete_message(
+                channel_id=self.message_get.channel_id,
+                message_id=self.message_get.id,
+                reason=reason,
+            )
+
+        async def edit(
+            self,
+            content: Optional[str] = None,
+            embeds: Optional[List[Embed]] = None,
+            flags: Optional[MessageFlag] = None,
+            allowed_mentions: Optional[AllowedMention] = None,
+            components: Optional[List[DirectComponent]] = None,
+            files: Optional[List[File]] = None,
+            attachments: Optional[List[AttachmentSend]] = None,
+        ) -> "DiscordReceipt":
+            mg = await cast(BotDiscord, self._get_bot()).edit_message(
+                channel_id=self.message_get.channel_id,
+                message_id=self.message_get.id,
+                content=content,
+                embeds=embeds,
+                flags=flags,
+                allowed_mentions=allowed_mentions,
+                components=components,
+                files=files,
+                attachments=attachments,
+            )
+            return self.__class__(message_get=mg, bot_id=self.bot_id)
+
+        async def pin(self, reason: Optional[str] = None):
+            return await cast(BotDiscord, self._get_bot()).pin_message(
+                channel_id=self.message_get.channel_id,
+                message_id=self.message_get.id,
+                reason=reason,
+            )
+
+        async def unpin(self, reason: Optional[str] = None):
+            return await cast(BotDiscord, self._get_bot()).unpin_message(
+                channel_id=self.message_get.channel_id,
+                message_id=self.message_get.id,
+                reason=reason,
+            )
+
+        async def react(self, emoji: str):
+            return await cast(BotDiscord, self._get_bot()).create_reaction(
+                channel_id=self.message_get.channel_id,
+                message_id=self.message_get.id,
+                emoji=emoji,
+            )
+
+        @property
+        def raw(self) -> MessageGet:
+            return self.message_get
+
+        def extract_message_id(self) -> DiscordMessageId:
+            return DiscordMessageId(
+                message_id=self.message_get.id, channel_id=self.message_get.channel_id
+            )
+
+    @register_sender(adapter)
     async def send(
-        bot: Bot,
-        msg: MessageFactory[MessageSegmentFactory],
+        bot,
+        msg: MessageFactory,
         target,
         event,
         at_sender: bool,
         reply: bool,
-    ):
-        assert isinstance(bot, Bot)
+    ) -> DiscordReceipt:
+        assert isinstance(bot, BotDiscord)
         assert isinstance(target, TargetDiscordChannel)
         if event:
             assert isinstance(event, MessageEvent)
             full_msg = assamble_message_factory(
                 msg,
                 Mention(event.get_user_id()),
-                Reply(event.message_id),
+                Reply(
+                    DiscordMessageId(
+                        message_id=event.message_id, channel_id=event.channel_id
+                    )
+                ),
                 at_sender,
                 reply,
             )
@@ -128,9 +234,17 @@ try:
         for message_segment_factory in full_msg:
             message_segment = await message_segment_factory.build(bot)
             message_to_send += message_segment
-        await bot.send_to(message=message_to_send, **target.arg_dict(bot))
+        resp = await bot.send_to(message=message_to_send, **target.arg_dict(bot))
+        return DiscordReceipt(message_get=resp, bot_id=bot.self_id)
 
-except ImportError:
-    pass
-except Exception as e:
-    raise e
+    @register_list_targets(adapter)
+    async def list_targets(bot: BaseBot) -> List[PlatformTarget]:
+        assert isinstance(bot, BotDiscord)
+        channel_ids: List[Snowflake] = []
+        guild_list = await bot.get_current_user_guilds()
+        for guild in guild_list:
+            channels = await bot.get_guild_channels(guild_id=guild.id)
+            for channel in channels:
+                channel_ids.append(channel.id)
+
+        return [TargetDiscordChannel(channel_id=channel.id) for channel in channels]
--- tests/test_discord.py
+++ tests/test_discord.py
@@ -1,87 +1,36 @@
-# ruff: noqa: E402
-import tempfile
 from io import BytesIO
 from pathlib import Path
-
-import httpx
-import pytest
-
-pytest.importorskip("nonebot.adapters.discord")
-
+from datetime import datetime
 from functools import partial
 
-import nonebot
+import httpx
+import respx
+import pytest
 from nonebug import App
 from nonebot import get_driver
-from nonebot.params import Event
-from nonebot.adapters.discord import Bot, Adapter
+from nonebot.adapters.discord import Bot
 from nonebot.adapters.discord.config import BotInfo
-from nonebot.adapters.discord.event import EventType
-from nonebot.adapters.discord.message import TextSegment
-from nonebot.adapters.discord.api import User, MessageFlag, MessageType
 
-from nonebot_plugin_saa.utils import SupportedAdapters
+from .utils import assert_ms, mock_discord_message_event
 
-from .utils import assert_ms
+discord_bot_info = BotInfo(token="123")
+discord_kwargs = {"self_id": "2333", "bot_info": discord_bot_info}
 
 
-@pytest.fixture(scope="session", autouse=True)
-def load_adapters(nonebug_init: None):
-    driver = nonebot.get_driver()
-    driver.register_adapter(Adapter)
+@pytest.fixture
+def assert_discord(app: App):
+    from nonebot_plugin_saa import SupportedAdapters
 
-
-def discord_kwargs():
-    return {"self_id": "2333", "bot_info": BotInfo(token=123)}
-
-
-def mock_discord_message_event(create: bool = True):
-    from nonebot.adapters.discord.event import MessageEvent as DiscordMessageEvent
-    from nonebot.adapters.discord.event import (
-        MessageCreateEvent as DiscordMessageCreateEvent,
+    return partial(
+        assert_ms,
+        Bot,
+        SupportedAdapters.discord,
+        bot_info=discord_bot_info,
+        self_id=discord_kwargs["self_id"],
     )
 
-    event_args = {
-        "id": 1234,
-        "channel_id": 4321,
-        "author": User(
-            **{
-                "id": 1234,
-                "username": ".canxin121",
-                "discriminator": "0",
-                "avatar": None,
-            }
-        ),
-        "content": "/test",
-        "timestamp": 1,
-        "edited_timestamp": None,
-        "tts": False,
-        "mention_everyone": False,
-        "mentions": [],
-        "mention_roles": [],
-        "attachments": [],
-        "embeds": [],
-        "nonce": 3210,
-        "pinned": False,
-        "type": MessageType(0),
-        "flags": MessageFlag(0),
-        "referenced_message": None,
-        "components": [],
-        "to_me": False,
-        "reply": None,
-        "_message": [TextSegment(type="text", data={"text": "/test"})],
-    }
 
-    if not create:
-        return DiscordMessageEvent(__type__=EventType.MESSAGE_CREATE, **event_args)
-    else:
-        return DiscordMessageCreateEvent(**event_args)
-
-
-assert_discord = partial(assert_ms, Bot, SupportedAdapters.discord, **discord_kwargs())
-
-
-async def test_text(app: App):
+async def test_text(app: App, assert_discord):
     from nonebot.adapters.discord import MessageSegment
 
     from nonebot_plugin_saa import Text
@@ -89,58 +38,54 @@ async def test_text(app: App):
     await assert_discord(app, Text("123"), MessageSegment.text("123"))
 
 
-async def test_image(app: App):
+@respx.mock
+async def test_image(app: App, assert_discord, tmp_path: Path):
     from nonebot.adapters.discord import MessageSegment
 
     from nonebot_plugin_saa import Image
 
-    data = BytesIO(b"\x89PNG\r")
+    image_route = respx.get("https://example.com/amiya.png")
+    image_route.mock(return_value=httpx.Response(200, content=b"amiya"))
+
+    data = b"\x89PNG\r"
     await assert_discord(
         app,
         Image(data),
-        MessageSegment.attachment(file="image.png", content=b"\x89PNG\r"),
+        MessageSegment.attachment(file="image", content=b"\x89PNG\r"),
     )
+
     data = BytesIO(b"\x89PNG\r")
     await assert_discord(
         app,
         Image(data.read()),
-        MessageSegment.attachment(file="image.png", content=b"\x89PNG\r"),
+        MessageSegment.attachment(file="image", content=b"\x89PNG\r"),
     )
 
-    with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp_file:
-        temp_file_path = Path(temp_file.name)
-        with open(temp_file_path, "wb") as f:
-            f.write(b"\x89PNG\r")
+    temp_image_path = tmp_path / "amiya.png"
+    with temp_image_path.open("wb") as f:
+        f.write(b"\x89PNG\r")
+
+    await assert_discord(
+        app,
+        Image(temp_image_path),
+        MessageSegment.attachment(file=temp_image_path.name, content=b"\x89PNG\r"),
+    )
+
+    await assert_discord(
+        app,
+        Image("https://example.com/amiya.png"),
+        MessageSegment.attachment(file="image", content=b"amiya"),
+    )
+
+    with pytest.raises(TypeError):
         await assert_discord(
             app,
-            Image(temp_file_path),
-            MessageSegment.attachment(file=temp_file_path.name, content=b"\x89PNG\r"),
-        )
-        await assert_discord(
-            app,
-            Image(str(temp_file_path)),
-            MessageSegment.attachment(file=temp_file_path.name, content=b"\x89PNG\r"),
+            Image(1),  # type: ignore
+            MessageSegment.attachment(file="1.png", content=b"\x89PNG\r"),
         )
 
-        async with httpx.AsyncClient() as client:
-            resp = await client.get("https://nonebot.dev/logo.png")
-            resp.raise_for_status()
-            data = resp.content
-        await assert_discord(
-            app,
-            Image(str("https://nonebot.dev/logo.png")),
-            MessageSegment.attachment(file="logo.png", content=data),
-        )
 
-    try:
-        await assert_discord(
-            app, Image(1), MessageSegment.attachment(file="1.png", content=b"\x89PNG\r")
-        )
-    except Exception:
-        pass
-
-
-async def test_mention(app: App):
+async def test_mention(app: App, assert_discord):
     from nonebot.adapters.discord import MessageSegment
 
     from nonebot_plugin_saa import Mention
@@ -148,17 +93,34 @@ async def test_mention(app: App):
     await assert_discord(app, Mention("123"), MessageSegment.mention_user(int("123")))
 
 
-async def test_reply(app: App):
+async def test_mention_all(app: App, assert_discord):
+    from nonebot.adapters.discord import MessageSegment
+
+    from nonebot_plugin_saa import MentionAll
+
+    await assert_discord(app, MentionAll(), MessageSegment.mention_everyone())
+
+
+async def test_reply(app: App, assert_discord):
     from nonebot.adapters.discord import MessageSegment
 
     from nonebot_plugin_saa import Reply
+    from nonebot_plugin_saa.adapters.discord import DiscordMessageId
 
-    await assert_discord(app, Reply("123"), MessageSegment.reference(int("123")))
+    await assert_discord(
+        app, Reply(DiscordMessageId(message_id=123)), MessageSegment.reference(123)
+    )
 
 
 async def test_send(app: App):
     from nonebot import on_message
     from nonebot.adapters.discord import Bot
+    from nonebot.adapters.discord.api.model import (
+        User,
+        Snowflake,
+        MessageGet,
+        MessageType,
+    )
 
     from nonebot_plugin_saa import Text, MessageFactory, SupportedAdapters
 
@@ -169,79 +131,72 @@ async def test_send(app: App):
         await MessageFactory(Text("123")).send()
 
     data = {
-        "data": {
-            "channel_id": 4321,
-            "nonce": None,
-            "tts": False,
-            "allowed_mentions": None,
-            "attachments": None,
-            "components": None,
-            "content": "123",
-            "embeds": None,
-            "files": None,
-            "message_reference": None,
-            "sticker_ids": None,
-        }
+        "channel_id": 4321,
+        "nonce": None,
+        "tts": False,
+        "allowed_mentions": None,
+        "content": "123",
     }
     async with app.test_matcher(matcher) as ctx:
         adapter_obj = get_driver()._adapters[str(SupportedAdapters.discord)]
-        bot = ctx.create_bot(base=Bot, adapter=adapter_obj, **discord_kwargs())
+        bot = ctx.create_bot(base=Bot, adapter=adapter_obj, **discord_kwargs)
         msg_event = mock_discord_message_event()
         ctx.receive_event(bot, msg_event)
-        ctx.should_call_api("create_message", **data)
-
-    async with app.test_matcher(matcher) as ctx:
-        adapter_obj = get_driver()._adapters[str(SupportedAdapters.discord)]
-        bot = ctx.create_bot(base=Bot, adapter=adapter_obj, **discord_kwargs())
-        msg_event = mock_discord_message_event(False)
-        ctx.receive_event(bot, msg_event)
-        ctx.should_call_api("create_message", **data)
-
-
-async def test_send_to(app: App):
-    from nonebot import on_message
-    from nonebot.adapters.discord import Bot
-
-    from nonebot_plugin_saa import (
-        Text,
-        MessageFactory,
-        SupportedAdapters,
-        TargetDiscordChannel,
-    )
-
-    matcher = on_message()
-
-    @matcher.handle()
-    async def process(event: Event, bot: Bot):
-        await MessageFactory(Text("123")).send_to(
-            TargetDiscordChannel(channel_id=event.channel_id), bot=bot
+        ctx.should_call_api(
+            "create_message",
+            data=data,
+            result=MessageGet(
+                id=Snowflake(1234),
+                channel_id=Snowflake(4321),
+                author=User(
+                    id=Snowflake(1234),
+                    username="canxin121",
+                    discriminator="0",
+                    avatar=None,
+                    global_name=None,
+                ),
+                content="/test",
+                timestamp=datetime(year=1999, month=9, day=9),
+                edited_timestamp=None,
+                tts=False,
+                mention_everyone=False,
+                mentions=[],
+                mention_roles=[],
+                attachments=[],
+                embeds=[],
+                pinned=False,
+                type=MessageType(0),
+            ),
         )
 
-    data = {
-        "data": {
-            "channel_id": 4321,
-            "nonce": None,
-            "tts": False,
-            "allowed_mentions": None,
-            "attachments": None,
-            "components": None,
-            "content": "123",
-            "embeds": None,
-            "files": None,
-            "message_reference": None,
-            "sticker_ids": None,
-        }
-    }
     async with app.test_matcher(matcher) as ctx:
         adapter_obj = get_driver()._adapters[str(SupportedAdapters.discord)]
-        bot = ctx.create_bot(base=Bot, adapter=adapter_obj, **discord_kwargs())
-        msg_event = mock_discord_message_event()
-        ctx.receive_event(bot, msg_event)
-        ctx.should_call_api("create_message", **data)
-
-    async with app.test_matcher(matcher) as ctx:
-        adapter_obj = get_driver()._adapters[str(SupportedAdapters.discord)]
-        bot = ctx.create_bot(base=Bot, adapter=adapter_obj, **discord_kwargs())
+        bot = ctx.create_bot(base=Bot, adapter=adapter_obj, **discord_kwargs)
         msg_event = mock_discord_message_event(False)
         ctx.receive_event(bot, msg_event)
-        ctx.should_call_api("create_message", **data)
+        ctx.should_call_api(
+            "create_message",
+            data=data,
+            result=MessageGet(
+                id=Snowflake(1234),
+                channel_id=Snowflake(4321),
+                author=User(
+                    id=Snowflake(1234),
+                    username="canxin121",
+                    discriminator="0",
+                    avatar=None,
+                    global_name=None,
+                ),
+                content="/test",
+                timestamp=datetime(year=1999, month=9, day=9),
+                edited_timestamp=None,
+                tts=False,
+                mention_everyone=False,
+                mentions=[],
+                mention_roles=[],
+                attachments=[],
+                embeds=[],
+                pinned=False,
+                type=MessageType(0),
+            ),
+        )
